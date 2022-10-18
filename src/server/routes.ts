0c04bc44a12156dfd10bd32fc217402fd8aaeef5
jasonkianhonting <jasonkianhonting@gmail.com>
2022-10-19 08:12:50 +1000

    Made more comments to explain further and cleared json file for a clean database like

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -20,7 +20,6 @@ router.get("/api/cheeses", async (req, res, next) => {
 
 //API route to post the items in the cart to be purchased into a JSON file in data/purchases.json
 router.post("/api/purchases", jsonParser, async (req, res) => {
-	
 	//PurchaseId being +1 due to the length starting from 0
 	const purchaseId = purchases.length + 1;
 
@@ -49,11 +48,11 @@ router.post("/api/purchases", jsonParser, async (req, res) => {
 					if (err) {
 						res.status(400).json(err.message);
 					} else {
-						res.status(200).json("Purchase was successful");
+						res.status(200).json("JSON File Was Successfully Created");
 					}
 				}
 			);
-			res.status(200).json("Purchase was successful");
+			res.status(200).json("Your Purchase Was Successful");
 		} catch (err) {
 			res.status(400).json(err.message);
 		}
@@ -64,7 +63,8 @@ router.post("/api/purchases", jsonParser, async (req, res) => {
 //error messages will be displayed
 
 router.get("/api/purchases", async (req, res, next) => {
-	//try catch statement for error handling
+	//try catch statement for error handling such that it fetches data from json file
+	//it then shows the correct status code if it is successful
 	try {
 		await res.json(purchases);
 		res.status(200);
